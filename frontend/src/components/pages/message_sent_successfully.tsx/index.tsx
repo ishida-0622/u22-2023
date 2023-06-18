a1d69f81dbc2c63eb26cd369e11d724ff416f0e9
rina suzuki <szk_rina@outlook.jp>
2023-06-18 09:53:08 +0900

    feat:UI部分完成

--- frontend/src/components/pages/message_sent_successfully.tsx/index.tsx
+++ frontend/src/components/pages/message_sent_successfully.tsx/index.tsx
@@ -1,5 +1,25 @@
-import { MessageSentSuccessfully } from "@/components/pages/message_sent_successfully.tsx";
+export const MessageSentSuccessfully = () => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-export default function message_sent_successfully() {
-    return <MessageSentSuccessfully />;
-}
+        const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
+        if (baseUrl === undefined) {
+            throw new Error("内部エラー");
+        }
+        // Signup画面の関数呼び出しする
+    };
+
+    return (
+        <div>
+            <form method="post" onSubmit={handleSubmit}>
+                <div>
+                    <p>メールを送信しました</p>
+                    <p>メールボックスを確認してください。</p>
+                </div>
+                <div>
+                    <button type="submit">メールを再送信する</button>
+                </div>
+            </form>
+        </div>
+    );
+};
