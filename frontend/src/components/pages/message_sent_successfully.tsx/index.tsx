994006b0b6240049f37723c5c1b608f12432874e
rina suzuki <szk_rina@outlook.jp>
2023-06-18 09:43:47 +0900

    feat:新規作成

---
+++ frontend/src/components/pages/message_sent_successfully.tsx/index.tsx
@@ -0,0 +1,5 @@
+import { MessageSentSuccessfully } from "@/components/pages/message_sent_successfully.tsx";
+
+export default function message_sent_successfully() {
+    return <MessageSentSuccessfully />;
+}
