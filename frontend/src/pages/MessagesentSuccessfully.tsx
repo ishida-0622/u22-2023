afe6f55221a68f743df6dc6ecaed1318d8bcbb15
rina suzuki <szk_rina@outlook.jp>
2023-06-18 18:55:42 +0900

    feat:ファイル・フォルダ名称変更

---
+++ frontend/src/pages/MessagesentSuccessfully.tsx
@@ -0,0 +1,5 @@
+import { MessageSentSuccessfully } from "@/components/pages/MessageSentSuccessfully";
+
+export default function MessagesentSuccessfully() {
+    return <MessageSentSuccessfully />;
+}
--- frontend/src/pages/message_sent_successfully.tsx
+++
@@ -1,5 +0,0 @@
-import { MessageSentSuccessfully } from "@/components/pages/message_sent_successfully.tsx";
-
-export default function message_sent_successfully() {
-    return <MessageSentSuccessfully />;
-}
