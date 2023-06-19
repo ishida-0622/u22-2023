d410e83d2c96f5b7b94fb6ddf7e8db1fa28ff317
rina suzuki <szk_rina@outlook.jp>
2023-06-19 12:34:57 +0900

    feat:onClickに変更（関数は未修正）

--- frontend/src/components/pages/MessageSentSuccessfully/index.tsx
+++ frontend/src/components/pages/MessageSentSuccessfully/index.tsx
@@ -1,6 +1,6 @@
 export const MessageSentSuccessfully = () => {
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const handleSubmit = async () => {
+        // event.preventDefault();
 
         const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
         if (baseUrl === undefined) {
@@ -11,15 +11,15 @@ export const MessageSentSuccessfully = () => {
 
     return (
         <div>
-            <form method="post" onSubmit={handleSubmit}>
-                <div>
-                    <p>メールを送信しました</p>
-                    <p>メールボックスを確認してください。</p>
-                </div>
-                <div>
-                    <button type="submit">メールを再送信する</button>
-                </div>
-            </form>
+            <div>
+                <p>メールを送信しました</p>
+                <p>メールボックスを確認してください。</p>
+            </div>
+            <div>
+                <button type="submit" onClick={handleSubmit}>
+                    メールを再送信する
+                </button>
+            </div>
         </div>
     );
 };
