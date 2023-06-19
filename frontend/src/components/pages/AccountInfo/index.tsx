cd86d318822f8790a5bb0d67651a4901656a33b4
kai-cham <yoshitomikai@softbank.jp>
2023-06-19 16:51:25 +0900

    しゅわーっ

--- frontend/src/components/pages/AccountInfo/index.tsx
+++ frontend/src/components/pages/AccountInfo/index.tsx
@@ -1,7 +1,23 @@
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+import "react-tabs/style/react-tabs.css";
+
 export const AccountInfo = () => {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>アカウント情報画面</h1>
+      <Tabs>
+        <TabList>
+          <Tab>アカウント情報</Tab>
+          <Tab>設定</Tab>
+          <Tab>パズルログ</Tab>
+          <Tab>えほんログ</Tab>
+        </TabList>
+        <TabPanel>a</TabPanel>
+        <TabPanel>b</TabPanel>
+        <TabPanel>c</TabPanel>
+        <TabPanel>d</TabPanel>
+      </Tabs>
+    </div>
+  );
+};
