75097e1e6112c8c449a7d1df8a1df23af6054f2d
rina suzuki <szk_rina@outlook.jp>
2023-06-26 16:44:06 +0900

    feat:UI部分完成

--- frontend/src/components/pages/PostAnnouncement/index.tsx
+++ frontend/src/components/pages/PostAnnouncement/index.tsx
@@ -1,97 +1,127 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import { NowLoading } from "@/components/elements/NowLoading";
 import Link from "next/link";
 
+// Modalを表示するHTML要素のidを指定
+Modal.setAppElement("#__next");
+
 export const PostAnnouncement = () => {
-    const [formValues, setFormValues] = useState({
-        title: "",
-        content: "",
-        name: "",
-    });
+  const [formValues, setFormValues] = useState({
+    title: "",
+    content: "",
+    name: "",
+  });
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  //モーダルウィンドウの表示/非表示を表すbool値を宣言
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  /** モーダルウィンドウを表示にする関数 */
+  const openModal = () => setModalIsOpen(true);
+  /** モーダルウィンドウを非表示にする関数 */
+  const closeModal = () => setModalIsOpen(false);
 
-        const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
-        if (baseUrl === undefined) {
-            throw new Error("内部エラー");
-        }
-        try {
-            const response = await fetch(`${baseUrl}/auth/signup`, {
-                method: "POST",
-                body: JSON.stringify({
-                    formValues,
-                }),
-            });
-            ScreenTransition();
-        } catch (e) {
-            alert("作成に失敗しました");
-        }
-    };
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    openModal();
+  };
 
-    const router = useRouter();
+  const sendNews = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
+    if (baseUrl === undefined) {
+      throw new Error("内部エラー");
+    }
+    try {
+      const response = await fetch(`${baseUrl}/auth/postannounce`, {
+        method: "POST",
+        body: JSON.stringify({
+          formValues,
+        }),
+      });
+      ScreenTransition();
+    } catch (e) {
+      alert("作成に失敗しました");
+    }
+  };
 
-    const ScreenTransition = () => {
-        router.push("/announcement-page");
-    };
+  const router = useRouter();
 
-    return (
+  const ScreenTransition = () => {
+    router.push("/announcement-page");
+  };
+
+  return (
+    <main>
+      <div>
+        <h2>お知らせ投稿</h2>
+        <hr />
+        <form method="post" onSubmit={handleSubmit}>
+          <div>
+            <label>
+              題名
+              <input
+                type="text"
+                name="title"
+                id="title"
+                value={formValues.title}
+                onChange={(e) =>
+                  setFormValues((val) => ({
+                    ...val,
+                    title: e.target.value,
+                  }))
+                }
+                required={true}
+              />
+            </label>
+            <label>
+              内容
+              <textarea
+                name="content"
+                id="content"
+                value={formValues.content}
+                onChange={(e) =>
+                  setFormValues((val) => ({
+                    ...val,
+                    content: e.target.value,
+                  }))
+                }
+                required={true}
+              ></textarea>
+            </label>
+            <label>
+              投稿者名
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={formValues.name}
+                onChange={(e) =>
+                  setFormValues((val) => ({
+                    ...val,
+                    name: e.target.value,
+                  }))
+                }
+                required={true}
+              />
+            </label>
+            <button type="submit">投稿内容を確認</button>
+            <Link href="/announcement-page">投稿一覧ページへ戻る</Link>
+          </div>
+        </form>
+      </div>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
         <div>
-            <h2>お知らせ投稿</h2>
-            <hr />
-            <form method="post" onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        題名
-                        <input
-                            type="text"
-                            name="title"
-                            id="title"
-                            value={formValues.title}
-                            onChange={(e) =>
-                                setFormValues((val) => ({
-                                    ...val,
-                                    title: e.target.value,
-                                }))
-                            }
-                            required={true}
-                        />
-                    </label>
-                    <label>
-                        内容
-                        <textarea
-                            name="content"
-                            id="content"
-                            value={formValues.content}
-                            onChange={(e) =>
-                                setFormValues((val) => ({
-                                    ...val,
-                                    content: e.target.value,
-                                }))
-                            }
-                            required={true}
-                        ></textarea>
-                    </label>
-                    <label>
-                        投稿者名
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            value={formValues.name}
-                            onChange={(e) =>
-                                setFormValues((val) => ({
-                                    ...val,
-                                    name: e.target.value,
-                                }))
-                            }
-                            required={true}
-                        />
-                    </label>
-                    <button type="submit">投稿内容を確認</button>
-                    <Link href="/announcement-page">投稿一覧ページへ戻る</Link>
-                </div>
-            </form>
+          <h2>投稿内容を確認してください。</h2>
+          <div>題名:「{formValues.title}」</div>
+          <div>投稿内容：「{formValues.content}」</div>
+          <div>投稿者名：「{formValues.name}」</div>
+          <button type="button" onClick={sendNews}>
+            投稿する
+          </button>
         </div>
-    );
+      </Modal>
+    </main>
+  );
 };
