da1c92c1f115b3ac7e33435ea3c4857351ef922f
manato miura <mmiura2351@gmail.com>
2023-06-26 09:01:24 +0900

    feat: Utilsの追加

---
+++ backend/app/src/main/kotlin/tic/u22/Utils.kt
@@ -0,0 +1,61 @@
+package tic.u22
+
+import aws.sdk.kotlin.services.dynamodb.*
+import aws.sdk.kotlin.services.dynamodb.endpoints.*
+import aws.sdk.kotlin.services.dynamodb.model.*
+import aws.sdk.kotlin.services.dynamodb.paginators.*
+import aws.sdk.kotlin.services.dynamodb.waiters.*
+import aws.sdk.kotlin.services.lambda.*
+import kotlin.reflect.*
+import kotlin.reflect.full.*
+
+class Utils {
+    /**
+     * objectをMapに変換する
+     *
+     * User("1", "ken", 20) の場合は
+     *
+     * { id: "1", name: "ken", age: 20 } に変換される
+     *
+     * @param obj 変換したいオブジェクト
+     *
+     * @return 変換されたMap
+     */
+    fun <T : Any> toMap(obj: T): Map<String, Any?> {
+        return (obj::class as KClass<T>).memberProperties.associate { prop ->
+            prop.name to
+                prop.get(obj)?.let { value ->
+                if (value::class.isData) {
+                    toMap(value)
+                } else {
+                    value
+                }
+            }
+        }
+    }
+
+    /**
+     * MapのvalueをAttributeValue型に変換する
+     *
+     * @param mp 変換したいMap
+     *
+     * @return 変換されたMap
+     */
+    fun toAttributeValueMap(mp: Map<String, Any?>): Map<String, AttributeValue> {
+        return mp
+            .map { it ->
+            val value = it.value
+            val resValue: AttributeValue =
+                when (value) {
+                    is String -> AttributeValue.S(value)
+                    is Int, is Long -> AttributeValue.N(value.toString())
+                    is Boolean -> AttributeValue.Bool(value)
+                    null -> AttributeValue.Null(true)
+                    is ByteArray -> AttributeValue.B(value)
+                    else -> AttributeValue.S(value as String)
+                }
+            it.key to resValue
+        }
+        .toMap()
+    }
+}
