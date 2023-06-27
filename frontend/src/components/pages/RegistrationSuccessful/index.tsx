50bf79a37e4ba8b952703409fc9b88d5115f7de6
rina suzuki <szk_rina@outlook.jp>
2023-06-27 12:08:43 +0900

    fix:インデント

--- frontend/src/components/pages/RegistrationSuccessful/index.tsx
+++ frontend/src/components/pages/RegistrationSuccessful/index.tsx
@@ -1,26 +1,26 @@
 import { useRouter } from "next/router";
 
 export const RegistrationSuccessful = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const PageTransition = () => {
-        router.push("/login");
-    };
+  const PageTransition = () => {
+    router.push("/login");
+  };
 
-    const handleSubmit = async () => {
-        PageTransition();
-    };
+  const handleSubmit = async () => {
+    PageTransition();
+  };
 
-    return (
-        <div>
-            <div>
-                <p>登録が完了しました</p>
-            </div>
-            <div>
-                <button type="submit" onClick={handleSubmit}>
-                    ログインする
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <p>登録が完了しました</p>
+      </div>
+      <div>
+        <button type="submit" onClick={handleSubmit}>
+          ログインする
+        </button>
+      </div>
+    </div>
+  );
 };
