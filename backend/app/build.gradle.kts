4e58430e4952ed22ccce6c6d8d66c7c6c32fe2b7
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-06-29 12:22:00 +0900

    feat: add module gson

--- backend/app/build.gradle.kts
+++ backend/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
+    implementation ("com.google.code.gson:gson:2.8.6")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
