8cb089f5516ebbdeee57479a223bcb924aeae770
rina suzuki <szk_rina@outlook.jp>
2023-06-29 16:37:59 +0900

    feat:UI完成

--- frontend/src/components/pages/adminTop/index.tsx
+++ frontend/src/components/pages/adminTop/index.tsx
@@ -3,35 +3,43 @@ import { useRouter } from "next/router";
 export const Top = () => {
   const router = useRouter();
 
-  const ScreenTransition = (path) => {
-    router.push(path);
+  const logout = () => {
+    router.push("/login");
+  };
+
+  const book = () => {
+    router.push("/admin/book");
+  };
+
+  const puzzle = () => {
+    router.push("/admin/puzzle");
+  };
+
+  const sticker = () => {
+    router.push("/admin/sticker");
+  };
+
+  const news = () => {
+    router.push("/admin/news");
   };
 
   return (
     <div>
       <div>
-        <button onClick={() => ScreenTransition("/admin/logout")}>
-          ログアウト
-        </button>
+        <button onClick={logout}>ログアウト</button>
       </div>
       <div>
         <div>
-          <button onClick={() => ScreenTransition("/admin/book")}>本棚</button>
+          <button onClick={book}>本棚</button>
         </div>
         <div>
-          <button onClick={() => ScreenTransition("/admin/puzzle")}>
-            問題
-          </button>
+          <button onClick={puzzle}>問題</button>
         </div>
         <div>
-          <button onClick={() => ScreenTransition("/admin/sticker")}>
-            シール
-          </button>
+          <button onClick={sticker}>シール</button>
         </div>
         <div>
-          <button onClick={() => ScreenTransition("/admin/news")}>
-            お知らせ
-          </button>
+          <button onClick={news}>お知らせ</button>
         </div>
       </div>
     </div>
