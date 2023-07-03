1fcb2a05b38e07d3827f7445033f265543c7fc99
manato miura <mmiura2351@gmail.com>
2023-07-03 09:15:08 +0900

    feat: 本番環境用のコードをコメントアウトで実装

--- backend/app/src/main/kotlin/tic/u22/Auth.kt
+++ backend/app/src/main/kotlin/tic/u22/Auth.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.runBlocking
 import com.google.gson.Gson
 import com.google.gson.JsonParser
 
+// テスト用コードここから
 class App : RequestHandler<Map<String, String>, String> {
   override fun handleRequest(event: Map<String, String>?, context: Context?): String{
     val res = runBlocking {
@@ -24,6 +25,50 @@ class App : RequestHandler<Map<String, String>, String> {
       val password = if (event["password"] != null) {event["password"]!!} else {throw Exception("password is null")}
       val child_lock = if (event["child_lock"] != null) {event["child_lock"]!!} else {throw Exception("child_lock is null")}
       val account_name = if (event["account_name"] != null) {event["account_name"]!!} else {throw Exception("account_name is null")}
+      // テスト用コードここまで
+
+// 本番環境用コードここから
+/*
+// 解析したJSONを保持するデータクラス
+data class FormValues(
+    val familyname: String = "",
+    val firstname: String = "",
+    val familynameEng: String = "",
+    val firstnameEng: String = "",
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val passwordConfirm: String = "",
+    val child: String = "",
+    val childConfirm: String = "",
+    val consent: Boolean = false
+)
+
+class App : RequestHandler<Map<String, Any>, String> {
+  override fun handleRequest(event: Map<String, Any>?, context: Context?): String{
+    val res = runBlocking {
+      // eventが正しく送られてきているかを確認
+      if (event == null) {throw Exception("event is null")}
+
+      // JSONを解析する
+      val body = event["body"] as String
+      val jsonElement = JsonParser.parseString(body)
+      val formValuesJson = jsonElement.asJsonObject["formValues"]
+      val gson = Gson()
+      val formValues = gson.fromJson(formValuesJson, FormValues::class.java)
+
+      // ユーザーの情報を設定
+      val u_id = UUID.randomUUID().toString()
+      val family_name = if(formValues.familyname.isNotEmpty()) formValues.familyname else throw Exception("familynameが空です")
+      val first_name = if(formValues.firstname.isNotEmpty()) formValues.firstname else throw Exception("firstnameが空です")
+      val family_name_roma = if(formValues.familynameEng.isNotEmpty()) formValues.familynameEng else throw Exception("familynameEngが空です")
+      val first_name_roma = if(formValues.firstnameEng.isNotEmpty()) formValues.firstnameEng else throw Exception("firstnameEngが空です")
+      val email = if(formValues.email.isNotEmpty()) formValues.email else throw Exception("emailが空です")
+      val password = if(formValues.password.isNotEmpty()) formValues.password else throw Exception("passwordが空です")
+      val child_lock = if(formValues.child.isNotEmpty()) formValues.child else throw Exception("childが空です")
+      val account_name = if(formValues.username.isNotEmpty()) formValues.username else throw Exception("usernameが空です")
+      */
+      // 本番環境用コードここまで
       val user = User(
         u_id=u_id,
         family_name=family_name,
