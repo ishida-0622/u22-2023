a6a1623e00cf0642055f5bf1c31422d7e404d6bb
kai-cham <yoshitomikai@softbank.jp>
2023-07-10 15:52:04 +0900

    feat: パズルログ・えほんログの追加

--- frontend/src/components/pages/AccountInfo/index.tsx
+++ frontend/src/components/pages/AccountInfo/index.tsx
@@ -1,11 +1,53 @@
-import Script from "next/script";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 import "react-tabs/style/react-tabs.css";
 
 export const AccountInfo = () => {
   const [volume, setVolume] = useState(50);
+  const [selectedHour, setSelectedHour] = useState("00");
+  const [selectedMinute, setSelectedMinute] = useState("00");
+  const [puzzleLogs, setPuzzleLogs] = useState([
+    {
+      userId: "user",
+      puzzleId: "1234",
+      playCount: 4,
+      lastPlayTime: "2023-07-06T16:23:44Z",
+    },
+    {
+      userId: "user",
+      puzzleId: "1235",
+      playCount: 100,
+      lastPlayTime: "2023-07-06T16:23:44Z",
+    },
+  ]);
+
+  const [bookLogs, setBookLogs] = useState([
+    {
+      userId: "user",
+      bookId: "1234",
+      playCount: 4,
+      lastReadTime: "2023-07-06T16:23:44Z",
+    },
+    {
+      userId: "user",
+      bookId: "1235",
+      playCount: 100,
+      lastReadTime: "2023-07-06T16:23:44Z",
+    },
+  ]);
+
+  const handleHourChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSelectedHour(event.target.value);
+  };
+
+  const handleMinuteChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSelectedMinute(event.target.value);
+  };
 
   return (
     <div>
@@ -27,7 +69,7 @@ export const AccountInfo = () => {
           <input type="password" name="passward" readOnly={true} />
           <p>チャイルドロック</p>
           <input type="password" name="chilglock" readOnly={true} />
-          <button type="button" name="change">
+          <button type="button" name="account_change">
             アカウント情報を変更
           </button>
         </TabPanel>
@@ -46,18 +88,64 @@ export const AccountInfo = () => {
             <input type="radio" name="font-size" />
           </label>
           <p>音量</p>
-          <input
-            type="range"
-            name="unci"
-            id="korogunomi"
-            min={0}
-            max={100}
-            value={volume}
-            onChange={(e) => setVolume(Number(e.target.value))}
-          />
+          <div>
+            <input type="button" name="yellow_green" />
+            <input type="button" name="light_blue" />
+            <input type="button" name="blue" />
+            <input type="button" name="yellow" />
+            <input type="button" name="orange" />
+            <input type="button" name="red" />
+          </div>
+          <p>使用時間制限</p>
+          <select value={selectedHour} onChange={handleHourChange}>
+            {Array.from({ length: 24 }, (_, i) =>
+              i.toString().padStart(2, "0")
+            ).map((hour) => (
+              <option key={hour} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+          時間
+          <select value={selectedMinute} onChange={handleMinuteChange}>
+            {Array.from({ length: 60 }, (_, i) =>
+              i.toString().padStart(2, "0")
+            ).map((minute) => (
+              <option key={minute} value={minute}>
+                {minute}
+              </option>
+            ))}
+          </select>
+          分
+          <div>
+            <button type="button" name="setting_return">
+              戻る
+            </button>
+            <button type="button" name="setting_change">
+              変更
+            </button>
+          </div>
         </TabPanel>
-        <TabPanel>c</TabPanel>
-        <TabPanel>d</TabPanel>
+        <TabPanel>
+          パズルログ
+          {puzzleLogs.map((log) => (
+            <div key={log.puzzleId}>
+              <div>No.{log.puzzleId}</div>
+              <div>クリア回数：{log.playCount}</div>
+              <div>最終クリア時刻{log.lastPlayTime}</div>
+            </div>
+          ))}
+        </TabPanel>
+        <TabPanel>
+          えほんログ
+          {bookLogs.map((log) => (
+            <div key={log.bookId}>
+              <div>No.{log.bookId}</div>
+              <div>絵本タイトル：{log.playCount}</div>
+              <div>読んだ時間{log.lastReadTime}</div>
+            </div>
+          ))}
+        </TabPanel>
       </Tabs>
     </div>
   );
