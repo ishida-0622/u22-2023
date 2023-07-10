494896177d347f9f5da5e6f90ec70f69c77cc2a8
manato miura <mmiura2351@gmail.com>
2023-07-10 09:17:24 +0900

    fix: データの受け取り方を修正

--- backend/app/src/main/kotlin/tic/u22/Others.kt
+++ backend/app/src/main/kotlin/tic/u22/Others.kt
@@ -10,7 +10,13 @@ import kotlinx.coroutines.runBlocking
 
 import com.google.gson.Gson
 import com.google.gson.JsonParser
-
+/**
+ * 任意の個数のIDを受け取りユーザーの情報を取得する
+ *
+ * @param id List<String>: {
+ *
+ * return List<Map<String, AttributeValue>>
+ */
 class ScanUsers : RequestHandler<Map<String, Any>, String> {
   override fun handleRequest(event: Map<String, Any>?, context: Context?): String {
     val res = runBlocking {
@@ -22,13 +28,13 @@ class ScanUsers : RequestHandler<Map<String, Any>, String> {
       if (event["body"] == null) {throw Exception("body is null")}
       val body = utils.formatJsonEnv(event["body"]!!)
       if (body["id"] == null) {throw Exception("id is null")}
-      val id: List<List<String>> = if (body["id"] != null) {body["id"]!! as List<List<String>>} else {throw Exception("id is null")}
+      val id: List<String> = if (body["id"] != null) {body["id"]!! as List<String>} else {throw Exception("id is null")}
 
       // 検索
-      val users = dynamo.searchByKeys(tableName, id)
+      val users = dynamo.searchByKeys(tableName, id.map { listOf(it) })
       val res = mapOf("result" to users)
       res
     }
-    return Gson().toJson(res)
+    return gson.toJson(res)
   }
 }
