6297be748603b9c5803906382200ff49b08390f0
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-07-13 15:03:01 +0900

    feat: StartPuzzle and its API

--- backend/app/src/main/kotlin/tic/u22/Puzzle.kt
+++ backend/app/src/main/kotlin/tic/u22/Puzzle.kt
@@ -10,3 +10,49 @@ import kotlinx.coroutines.runBlocking
 
 import com.google.gson.Gson
 import com.google.gson.JsonParser
+
+class StartPuzzle : RequestHandler<Map<String, Any>, String> {
+    override fun handleRequest(event: Map<String, Any>?, context: Context?): String{
+
+        val res = runBlocking {
+            try{
+                if (event == null) {throw Exception("event is null")}
+                if (event["body"] == null) {throw Exception("body is null")}
+                val body = utils.formatJsonEnv(event["body"]!!)
+
+                val u_id = if (body["u_id"] != null) {body["u_id"]!! as String} else {throw Exception("u_id is null")}
+                val p_id = if (body["p_id"] != null) {body["p_id"]!! as String} else {throw Exception("p_id is null")}
+
+                // DynamoDBのインスタンス化、テーブル名の設定
+                val dynamo = Dynamo(Settings().AWS_REGION)
+
+                val status = dynamo.searchByKey("status", listOf(u_id))
+                if (!status.containsKey("u_id")) {
+                    throw Exception("this u_id does not exist")
+                } else if (!status.containsKey("game_status")) {
+                    throw Exception("unexpected error: this u_id does not game_status")
+                }
+                if((utils.toKotlinType(status["game_status"]!!) as String).toInt() != 0) {
+                    throw Exception("game status is not 0: now is ${(utils.toKotlinType(status["game_status"]!!) as String).toInt()}")
+                }
+
+                val updated = dynamo.updateItem("status", listOf(u_id), mapOf("game_status" to 1))
+                if(updated != "DONE"){
+                    throw Exception("failed to update game status: $updated")
+                }
+                val result = dynamo.searchByKey("puzzle", listOf(p_id))
+
+                mapOf(
+                    "resposne_status" to "success",
+                    "result" to utils.toMap(utils.attributeValueToObject(result, "puzzle"))
+                )
+            } catch (e: Exception) {
+                mapOf(
+                    "response_status" to "fail",
+                    "error" to "$e"
+                )
+            }
+        }
+        return gson.toJson(res)       // JSONに変換してフロントに渡す
+    }
+}
