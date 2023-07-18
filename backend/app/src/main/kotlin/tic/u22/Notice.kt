25471671b523010204d72441f0794a4007e561c8
manato miura <mmiura2351@gmail.com>
2023-07-18 11:21:50 +0900

    feat-ベースコードの実装

--- backend/app/src/main/kotlin/tic/u22/Notice.kt
+++ backend/app/src/main/kotlin/tic/u22/Notice.kt
@@ -10,3 +10,31 @@ import kotlinx.coroutines.runBlocking
 
 import com.google.gson.Gson
 import com.google.gson.JsonParser
+
+/**
+ * 
+ */
+
+class getNotice : RequestHandler<Map<String, Any>, String> {
+  override fun handleRequest(event: Map<String, Any>?, context: Context?): String {
+    val res = runBlocking {
+      try {
+        val dynamo = Dynamo(Settings().AWS_REGION)
+        val tableName = "notice"
+
+        val notices = dynamo.scanAll(tableName)
+        println(notices)
+        if (notices.isNotEmpty()) {
+          mapOf("response_status" to "success",
+          "result" to notices.map {
+            utils.toMap(utils.attributeValueToObject(it, tableName))})
+        } else {
+          mapOf("response_status" to "fail", "error" to "Failed to retrieve notices")
+        }
+      } catch(e: Exception){
+        mapOf("response_status" to "fail", "error" to "$e")
+      }
+    }
+    return gson.toJson(res)
+  }
+}
