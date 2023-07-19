678bd5d747fd195aae7be27aa6cf67a11992d0ad
kentaro ishida <ken.babuo@gmail.com>
2023-07-19 14:14:55 +0900

    feat: Reduxの設定の更新

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,10 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { userSlice } from "./user";
 
 const persistConfig = {
   key: "root",
+  version: 1,
   storage,
 };
 
@@ -12,6 +21,13 @@ const persistedReducer = persistReducer(persistConfig, userSlice.reducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
