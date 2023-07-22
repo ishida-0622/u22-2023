5395fc21e4d094dfffdb4e7b2921ec6a7f412d0b
kentaro ishida <ken.babuo@gmail.com>
2023-07-22 20:30:14 +0900

    clean: コメントアウトの削除

--- frontend/src/features/puzzle/types/get/index.ts
+++ frontend/src/features/puzzle/types/get/index.ts
@@ -3,9 +3,4 @@ import { Puzzle } from "@/features/puzzle/types";
 
 export type GetAllPuzzleRequest = {};
 
-export type GetAllPuzzleResponse = ApiResponse<
-  // Pick<
-  Puzzle[]
-  // "title" | "description" | "icon" | "words" | "create_date" | "update_date"
-  // >[]
->;
+export type GetAllPuzzleResponse = ApiResponse<Puzzle[]>;
