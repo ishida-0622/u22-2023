29584b6e65719ad2e0bf1cd70795846209089a73
mikomasa <mizuna0119@docomo.ne.jp>
2023-07-26 01:15:45 +0900

    style-ボタン、文字位置変更

--- frontend/src/components/pages/RegistrationSuccessful/index.tsx
+++ frontend/src/components/pages/RegistrationSuccessful/index.tsx
@@ -15,13 +15,13 @@ export const RegistrationSuccessful = () => {
   return (
     <div className={`${styles.container}`}>
       <div className={`${styles.child}`}>
-        <p className={`${styles.text}`}>登録が完了しました</p>
+        <h3>登録が完了しました</h3>
       <div className={`${styles.submit_button_field}`}>
         <button type="submit" onClick={handleSubmit} className={`${styles.submit_button}`} >
           ログインする
         </button>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
