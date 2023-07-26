0395a94f9a20ff0c630fe142510ced9dd08402cc
kentaro ishida <ken.babuo@gmail.com>
2023-07-27 02:47:08 +0900

    rename: ファイル名の変更

---
+++ frontend/src/components/pages/SendEmail/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { isLogin } from "@/features/auth/utils/isLogin";
+import Router from "next/router";
+import { sendEmailVerification } from "firebase/auth";
+import { auth } from "@/features/auth/firebase";
+
+export const SendEmail = () => {
+  const sendEmail = async () => {
+    const user = auth.currentUser;
+    if (!user) {
+      Router.push("/signup");
+      return;
+    }
+    sendEmailVerification(user);
+  };
+
+  useEffect(() => {
+    isLogin().then((res) => {
+      if (!res) {
+        Router.push("/signup");
+      } else {
+        const user = auth.currentUser;
+        if (user?.emailVerified) {
+          Router.push("/");
+        }
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <p>メールを送信しました</p>
+        <p>メールボックスを確認してください。</p>
+      </div>
+      <div>
+        <button onClick={sendEmail}>メールを再送信する</button>
+      </div>
+    </div>
+  );
+};
