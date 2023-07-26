e1246e7d2e050ae0d2d034d54054313dba5162d2
kentaro ishida <ken.babuo@gmail.com>
2023-07-27 01:52:19 +0900

    rename: ファイル名をsend-emailに変更、cssファイルの拡張子をscssに変更

--- frontend/src/pages/messagesent-successfully.tsx
+++
@@ -1,5 +0,0 @@
-import { MessageSentSuccessfully } from "@/components/pages/MessageSentSuccessfully";
-
-export default function MessagesentSuccessfully() {
-    return <MessageSentSuccessfully />;
-}
