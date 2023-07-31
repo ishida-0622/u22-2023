9f607e3ccb3702c91cb6f2714738c4ef5178d9d6
rina suzuki <szk_rina@outlook.jp>
2023-07-31 20:24:02 +0900

    fix:

--- frontend/src/components/pages/RegisterPuzzle/index.tsx
+++ frontend/src/components/pages/RegisterPuzzle/index.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-
 import Image from "next/image";
-
 import {
   RegisterPuzzleRequest,
   RegisterPuzzleResponse,
 } from "@/features/puzzle/types/register";
-
 import { PuzzleWord } from "@/features/puzzle/types";
+import { useRouter } from "next/router";
+import Modal from "react-modal";
+import Link from "next/link";
+
+// Modalを表示するHTML要素のidを指定
+Modal.setAppElement("#__next");
 
 export const RegisterPuzzle = () => {
   const [title, setTitle] = useState("");
@@ -23,15 +26,10 @@ export const RegisterPuzzle = () => {
   const [voices, setVoices] = useState<(string | null)[]>([]);
   const [isDisplayed, setIsDisplayed] = useState<boolean[]>([]);
   // ダミーのURIなど
-
   const [dummyWord, setDummyWord] = useState("");
-
   const [splitDummyWord, setSplitDummyWord] = useState<string[]>([]);
-
   const [dummyImages, setDummyImages] = useState<(string | null)[]>([]);
-
   const [dummyShadows, setDummyShadows] = useState<(string | null)[]>([]);
-
   const [dummyVoices, setDummyVoices] = useState<(string | null)[]>([]);
 
   const changeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,92 +44,59 @@ export const RegisterPuzzle = () => {
     const text = e.target.value;
 
     // 入力をセット
-
     setWord(text);
-
     if (text === "") {
       setSplitWord([]);
-
       setImages([]);
-
       setShadows([]);
-
       setVoices([]);
-
       setIsDisplayed([]);
-
       return;
     }
 
     // ,で区切って配列にする
-
     const arr = text.split(",");
-
     // 配列をセット
-
     setSplitWord(arr);
-
     // 配列の長さによって画像の入力欄を増減させる
-
     // concatとsliceはこちらを参考に
-
     // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
-
     // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
-
     setImages((val) => val.concat([null]).slice(0, arr.length));
-
     setShadows((val) => val.concat([null]).slice(0, arr.length));
-
     setVoices((val) => val.concat([null]).slice(0, arr.length));
-
     setIsDisplayed((val) => val.concat(false).slice(0, arr.length));
   };
 
   const dummyWordOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const text = e.target.value;
-
     setDummyWord(text);
-
     if (text === "") {
       setSplitDummyWord([]);
-
       setDummyImages([]);
-
       setDummyShadows([]);
-
       setDummyVoices([]);
-
       return;
     }
 
     const arr = text.split(",");
-
     setSplitDummyWord(arr);
-
     setDummyImages((val) => val.concat([null]).slice(0, arr.length));
-
     setDummyShadows((val) => val.concat([null]).slice(0, arr.length));
-
     setDummyVoices((val) => val.concat([null]).slice(0, arr.length));
   };
 
   const iconOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
-
     if (files && files.length > 0) {
       const f = files[0];
-
       const reader = new FileReader();
-
       reader.onload = (e) => {
         if (e.target === null) {
           return;
         }
-
         setIcon(e.target.result as string);
       };
-
       reader.readAsDataURL(f);
     } else {
       setIcon(null);
@@ -152,30 +117,22 @@ export const RegisterPuzzle = () => {
 
   const onChangeHandler = (
     e: React.ChangeEvent<HTMLInputElement>,
-
     index: number,
-
     updateFun: React.Dispatch<React.SetStateAction<(string | null)[]>>
   ) => {
     const files = e.target.files;
-
     if (files && files.length > 0) {
       const f = files[0];
-
       const reader = new FileReader();
-
       reader.onload = (e) => {
         if (e.target === null) {
           return;
         }
-
         updateFun((val) =>
           // indexが同じものだけを更新
-
           val.map((v, j) => (index === j ? (e.target!.result as string) : v))
         );
       };
-
       reader.readAsDataURL(f);
     } else {
       updateFun((val) => val.map((v, j) => (index === j ? null : v)));
@@ -184,35 +141,27 @@ export const RegisterPuzzle = () => {
 
   const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const reg = new RegExp(
       /^([a-zA-Z]+(\s[a-zA-Z]|[a-zA-Z])*)+(?:,([a-zA-Z]+(\s[a-zA-Z]|[a-zA-Z])*)+)*$/
     );
-
     // 問題文がカンマ区切りの英文かを判定
-
     if (!reg.test(word)) {
       alert("問題文の形式が不正です");
-
       return;
     }
 
     if (!(dummyWord === "" || reg.test(dummyWord))) {
       alert("ダミー文の形式が不正です");
-
       return;
     }
 
     // iconがnullかを判定
-
     if (!icon) {
       alert("アイコンをアップロードしてください");
-
       return;
     }
 
     // 画像と音声にnullが含まれているかを判定
-
     if (
       images.some((v) => v === null) ||
       shadows.some((v) => v === null) ||
@@ -222,22 +171,16 @@ export const RegisterPuzzle = () => {
       dummyVoices.some((v) => v === null)
     ) {
       alert("画像と音声をアップロードしてください");
-
       return;
     }
 
     const words: PuzzleWord[] = splitWord.map((v, i) => {
       return {
         word: v,
-
         shadow: shadows[i]!,
-
         illustration: images[i]!,
-
         voice: voices[i]!,
-
         is_displayed: isDisplayed[i],
-
         is_dummy: false,
       };
     });
@@ -245,15 +188,10 @@ export const RegisterPuzzle = () => {
     const dummyWords: PuzzleWord[] = splitDummyWord.map((v, i) => {
       return {
         word: v,
-
         shadow: dummyShadows[i]!,
-
         illustration: dummyImages[i]!,
-
         voice: dummyVoices[i]!,
-
         is_displayed: false,
-
         is_dummy: true,
       };
     });
@@ -273,15 +211,11 @@ export const RegisterPuzzle = () => {
 
     try {
       // 登録処理
-
       const res = await fetch(`${baseUrl}/RegisterPuzzle`, {
         method: "POST",
-
         body: JSON.stringify(req),
       });
-
       const json: RegisterPuzzleResponse = await res.json();
-
       if (json.response_status === "fail") {
         throw new Error(json.error);
       }
