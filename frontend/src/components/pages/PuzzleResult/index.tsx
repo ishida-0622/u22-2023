0b4d6724a3967e1a2917410200b18b285b418f54
kentaro ishida <ken.babuo@gmail.com>
2023-08-04 00:03:00 +0900

    clean: フォーマッターの適用、コメントの削除

--- frontend/src/components/pages/PuzzleResult/index.tsx
+++ frontend/src/components/pages/PuzzleResult/index.tsx
@@ -2,7 +2,7 @@ import Router, { useRouter } from "next/router";
 import Image from "next/image";
 import styles from "./index.module.scss";
 import { Menubar } from "@/components/elements/Menubar";
-import backGround from "@/components/pages/PuzzleResult/images/confetti.gif"
+import backGround from "@/components/pages/PuzzleResult/images/confetti.gif";
 
 export const PuzzleResult = () => {
   const router = useRouter();
@@ -20,10 +20,12 @@ export const PuzzleResult = () => {
   // }
 
   return (
-    <div className={`${styles.container}`} >
-
-      <Image className={`${styles.back_ground}`} src={backGround} alt="背景画像" />
-
+    <div className={`${styles.container}`}>
+      <Image
+        className={`${styles.back_ground}`}
+        src={backGround}
+        alt="background image"
+      />
       <h1 className={`${styles.big_message}`}>congratulation！</h1>
       {/* TODO:CSS完了後書き換え */}
       <div className={`${styles.seal_field}`}>
@@ -33,15 +35,12 @@ export const PuzzleResult = () => {
             "https://k-ishida-u22-2023-mock.s3.ap-northeast-1.amazonaws.com/IMG_3yvjyu.jpg"
           }
           alt="seal"
-          // UI班へ
-          // width, heightは必要に応じて書き換えてよいです
           width={230}
           height={230}
         />
       </div>
       {/* <Image src={imageUrl} alt="seal" width={100} height={100} /> */}
       <p className={`${styles.small_message}`}>You got a seal！</p>
-      {/* TODO:フッター */}
       <Menubar />
     </div>
   );
