c3b2f191ed18187146c667f755e08311e2261e85
Arai Shin <tky2202006@stu.o-hara.ac.jp>
2023-08-12 22:45:15 +0900

    feat:FinishBook

--- backend/app/src/main/kotlin/tic/u22/Book.kt
+++ backend/app/src/main/kotlin/tic/u22/Book.kt
@@ -243,3 +243,57 @@ class PauseBook : RequestHandler<Map<String, Any>, String> {
         return gson.toJson(res)
     }
 }
+
+/**
+ * u_id, b_idを受け取りゲームステータスの変更、ログの追加を行う
+ * 
+ * @param event Map<String, Any>?: "u_id": "u_id", "b_id": "b_id"
+ * 
+ * return String : {"response_status": "success", "result": {}}
+ */
+class FinishBook : RequestHandler<Map<String, Any>, String> {
+    override fun handleRequest(event: Map<String, Any>?, context: Context?): String {
+        val res = runBlocking {
+            try {
+                if (event == null) {throw Exception("event is null")}           // event引数のnullチェック
+                if (event["body"] == null) {throw Exception("body is null")}    // bodyのnullチェック
+                val body = utils.formatJsonEnv(event["body"]!!)                 // bodyをMapオブジェクトに変換
+
+                val u_id = if (body["u_id"] != null) {body["u_id"]!! as String} else {throw Exception("u_id is null")}
+                val b_id = if (body["b_id"] != null) {body["b_id"]!! as String} else {throw Exception("b_id is null")}
+
+                // DynamoDBのインスタンス化、テーブル名の設定
+                val dynamo = Dynamo(Settings().AWS_REGION)
+                val table_b_log = "b_log"
+                val table_status = "status"
+
+                val log = dynamo.searchByKey(table_b_log, listOf(u_id, b_id))
+                val playTimes = if (log["play_times"] != null) {
+                    (utils.toKotlinType(log["play_times"]!!) as String).toInt() + 1
+                } else {
+                    1
+                }                
+                val updated = dynamo.updateItem(table_status, listOf(u_id), mapOf("game_status" to 0)) // game_statusを0に変更
+                val b_log = BookLog(
+                    u_id = u_id,
+                    b_id = b_id,
+                    play_times = playTimes
+                )
+                // 初プレイ時にはログの追加それ以外はプレイ回数の増加
+                if (playTimes == 1) { dynamo.addItem(table_b_log, b_log) }
+                else { dynamo.updateItem(table_b_log, listOf(u_id, b_id), mapOf("play_times" to playTimes)) }
+
+                if(updated == "DONE"){
+                    val dummyMap: Map<String, String> = mapOf()
+                    mapOf("response_status" to "success", "result" to dummyMap)
+                } else {
+                    throw Exception("failed to update game status or failed to update log")
+                }
+            }
+            catch(e: Exception) {
+                mapOf("response_status" to "fail", "error" to "$e")
+            }
+        }
+        return gson.toJson(res)
+    }
+}
