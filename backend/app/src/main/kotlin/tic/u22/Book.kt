d6d4569056e457e1330d4b4dd2f5e82d7e0f0b3e
MiuraManato <manato20030510@icloud.com>
2023-08-16 02:15:48 +0900

    feat: Create RestartBook

--- backend/app/src/main/kotlin/tic/u22/Book.kt
+++ backend/app/src/main/kotlin/tic/u22/Book.kt
@@ -243,3 +243,64 @@ class PauseBook : RequestHandler<Map<String, Any>, String> {
         return gson.toJson(res)
     }
 }
+
+/**
+ * 読み聞かせを再開する
+ * 
+ * @param u_id String: u_id
+ * 
+ * return String: {"response_status": "success", "result": {}}
+ */
+class RestartBook : RequestHandler<Map<String, Any>, String> {
+    override fun handleRequest(event: Map<String, Any>?, context: Context?): String {
+        val res = runBlocking {
+            try {
+                if (event == null) {throw Exception("event is null")}
+                if (event["body"] == null) {throw Exception("body is null")}
+                val body = utils.formatJsonEnv(event["body"]!!)
+                val u_id = if (body["u_id"] != null) {body["u_id"]!! as String} else {throw Exception("u_id is null")}
+
+                val dynamo = Dynamo(Settings().AWS_REGION)
+                val table_status = "status"
+                val table_book = "book"
+
+                // ユーザーのステータスを取得
+                val current_status = dynamo.searchByKey(table_status, listOf(u_id))
+                if (!current_status.containsKey("u_id")) {throw Exception("this u_id does not exist")} 
+                if (!current_status.containsKey("game_status")) {throw Exception("unexpected error: this u_id does not game_status")}
+
+                // 現在のステータス判定
+                if((utils.toKotlinType(current_status["game_status"]!!) as String).toInt() != 4) {
+                    throw Exception("game status is not 4: now is ${(utils.toKotlinType(current_status["game_status"]!!) as String).toInt()}")
+                }
+
+                // ステータスの情報を取り出す
+                val game_infos = if (current_status["status_infos"] != null ) {
+                    utils.toKotlinType(current_status["status_infos"]!!) as List<String>
+                } else {
+                    throw Exception("status_infos is null")
+                }
+                val b_id = if (game_infos[0].isNotEmpty()) {game_infos[0]} else {throw Exception("b_id is null")}
+                val saved_data = if (game_infos[1].isNotEmpty()) {game_infos[1]} else {throw Exception("saved_data is null")}
+
+                val book_info = dynamo.searchByKey(table_book, listOf(b_id))
+                if (book_info.isEmpty()) {throw Exception("this book is not exist")}
+
+                // ステータスの更新
+                val updated = dynamo.updateItem(table_status, listOf(u_id), mapOf("game_status" to 3))
+                if (updated != "DONE"){throw Exception("failed to update game status: $updated")}
+
+                mapOf(
+                    "response_status" to "success",
+                    "result" to mapOf(
+                        "book_info" to utils.toMap(utils.attributeValueToObject(book_info, "book")),
+                        "saved_data" to saved_data
+                    )
+                )
+            } catch (e: Exception) {
+                mapOf("response_status" to "fail", "error" to "$e")
+            }
+        }
+        return gson.toJson(res)
+    }
+}
