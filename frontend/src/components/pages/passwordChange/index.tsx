d995c2e47113152bb17b31558a8cff983b1f1e18
kentaro ishida <ken.babuo@gmail.com>
2023-08-16 15:33:04 +0900

    feat: ロジック部分の実装

--- frontend/src/components/pages/passwordChange/index.tsx
+++ frontend/src/components/pages/passwordChange/index.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { updatePassword } from "firebase/auth";
+import { auth } from "@/features/auth/firebase";
+import { logout } from "@/features/auth/utils/logout";
+
 import styles from "./index.module.scss";
 
 export const PasswordChange = () => {
+  const router = useRouter();
+
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [isHidden, setIsHidden] = useState({ pass: true, passConfirm: true });
 
   const changePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
@@ -16,34 +25,30 @@ export const PasswordChange = () => {
     setPasswordConfirm(event.target.value);
   };
 
-  const router = useRouter();
-
-  const PageTransition = () => {
+  const toLoginPage = () => {
     router.push("/login");
   };
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
-    if (baseUrl === undefined) {
-      throw new Error("内部エラー");
+    if (password !== passwordConfirm) {
+      alert("パスワードが一致しません");
+      return;
     }
-    try {
-      const response = await fetch(`${baseUrl}/auth/password-change`, {
-        method: "POST",
-        body: JSON.stringify({
-          password: password,
-          passwordConfirm: passwordConfirm,
-        }),
+    const user = auth.currentUser;
+    if (!user) {
+      toLoginPage();
+      return;
+    }
+    updatePassword(user, password)
+      .then(() => {
+        alert("再設定が完了しました\nもう一度ログインしてください");
+        logout();
+      })
+      .catch((e) => {
+        alert("再設定に失敗しました");
+        console.error(e);
       });
-      // responseの処理
-      // responseがtrueの時、画面遷移をする
-      // responseがfalseの時、アラートを出す。
-      PageTransition();
-    } catch (e) {
-      alert("データの送信に失敗しました");
-    }
   };
 
   return (
@@ -56,25 +61,35 @@ export const PasswordChange = () => {
             <p className={`${styles.password_text}`}>新しいパスワード</p>
             <input
               className={`${styles.password_form}`}
-              type="password"
+              type={isHidden.pass ? "password" : "text"}
               name="password"
               id="password"
               value={password}
               onChange={changePassword}
               required={true}
             />
+            <FontAwesomeIcon
+              icon={isHidden.pass ? faEye : faEyeSlash}
+              onClick={() => setIsHidden((v) => ({ ...v, pass: !v.pass }))}
+            />
           </label>
           <br></br>
           <label>
-            <p className={`${styles.password_retype_field}`}>確認用</p>
+            <p className={`${styles.password_retype_field}`}>再入力</p>
             <input
               className={`${styles.password_retype_form}`}
-              type="password"
+              type={isHidden.passConfirm ? "password" : "text"}
               name="passwordConfirm"
               id="passwordConfirm"
               value={passwordConfirm}
               onChange={changePasswordConfirm}
               required={true}
+            />
+            <FontAwesomeIcon
+              icon={isHidden.passConfirm ? faEye : faEyeSlash}
+              onClick={() =>
+                setIsHidden((v) => ({ ...v, passConfirm: !v.passConfirm }))
+              }
             />
           </label>
         </div>
