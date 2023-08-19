8ebf3f2fb102e9b77c619c813948f7c56a95f406
kentaro ishida <ken.babuo@gmail.com>
2023-08-20 06:06:43 +0900

    feat: queryにimage urlが無い場合はトップに飛ばすように変更

--- frontend/src/components/pages/PuzzleResult/index.tsx
+++ frontend/src/components/pages/PuzzleResult/index.tsx
@@ -3,21 +3,17 @@ import Image from "next/image";
 import styles from "./index.module.scss";
 import { Menubar } from "@/components/elements/Menubar";
 import backGround from "@/components/pages/PuzzleResult/images/confetti.gif";
+import { useEffect } from "react";
 
 export const PuzzleResult = () => {
   const router = useRouter();
   const { imageUrl } = router.query;
 
-  // TODO:CSS完了後書き換え
-  // if (typeof imageUrl !== "string") {
-  //   // TODO:404
-  //   return (
-  //     <main id="notfound">
-  //       <h1>404 Not Found.</h1>
-  //       <button onClick={() => Router.push("/")}>to Top</button>
-  //     </main>
-  //   );
-  // }
+  useEffect(() => {
+    if (typeof imageUrl !== "string") {
+      Router.push("/");
+    }
+  }, []);
 
   return (
     <div className={`${styles.container}`}>
@@ -27,19 +23,15 @@ export const PuzzleResult = () => {
         alt="background image"
       />
       <h1 className={`${styles.big_message}`}>congratulation！</h1>
-      {/* TODO:CSS完了後書き換え */}
       <div className={`${styles.seal_field}`}>
         <Image
           className={`${styles.got_seal}`}
-          src={
-            "https://k-ishida-u22-2023-mock.s3.ap-northeast-1.amazonaws.com/IMG_3yvjyu.jpg"
-          }
+          src={imageUrl as string}
           alt="seal"
           width={230}
           height={230}
         />
       </div>
-      {/* <Image src={imageUrl} alt="seal" width={100} height={100} /> */}
       <p className={`${styles.small_message}`}>You got a seal！</p>
       <Menubar />
     </div>
