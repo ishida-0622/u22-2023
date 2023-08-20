2e266395dd8a921becfd370a9d119af387c46b0a
maruosan <09sasa.san@gmail.com>
2023-08-20 14:47:04 +0900

    style: トップ画面のCSS

--- frontend/src/components/pages/Top/index.tsx
+++ frontend/src/components/pages/Top/index.tsx
@@ -11,11 +11,11 @@ export const Top = () => {
     }
     function previousMonth() {
         if (addMonth > -3)
-        setAddMonth(addMonth - 1);
+            setAddMonth(addMonth - 1);
     }
     const [addMonth, setAddMonth] = useState(0);
     return (
-        <div>
+        <div className={`${styles.container}`}>
             {/* カレンダー置く場所 */}
             <div id="calendar">
                 <Calendar mm={addMonth} />
@@ -23,8 +23,10 @@ export const Top = () => {
                     これ以上さかのぼれない場合previousButtonInvalidにクラス名が変わります
                     nextButtonは次の月を表示するためのボタン
                     previousと同様にこれ以上進めない場合Invalidが付きます */}
-                <button id="previousMonth" className={addMonth == -3 ? styles.previousButtonInvalid : styles.previousButton} onClick={previousMonth}>←</button>
-                <button id="nextMonth" className={addMonth == 0 ? styles.nextButtonInvalid : styles.nextButton} onClick={nextMonth}>→</button>
+                <div className={`${styles.turn_calender}`}>
+                    <button id="previousMonth" className={addMonth == -3 ? styles.previousButtonInvalid : styles.previousButton} onClick={previousMonth}>←</button>
+                    <button id="nextMonth" className={addMonth == 0 ? styles.nextButtonInvalid : styles.nextButton} onClick={nextMonth}>→</button>
+                </div>
             </div>
             {/* タップでアカウント情報画面へ飛ぶアイコン(右上) */}
             <img
@@ -33,6 +35,7 @@ export const Top = () => {
                 onClick={() => {
                     Router.push("#");
                 }}
+                className={`${styles.icon}`}
             ></img>
             {/* お知らせ置く場所 */}
             <div id="information" className={styles.information}>
