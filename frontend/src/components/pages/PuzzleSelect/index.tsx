36acc1918c362a990434aac874a639a1e14f219f
kentaro ishida <ken.babuo@gmail.com>
2023-08-20 23:56:48 +0900

    clean: importの順序を変更

--- frontend/src/components/pages/PuzzleSelect/index.tsx
+++ frontend/src/components/pages/PuzzleSelect/index.tsx
@@ -1,35 +1,33 @@
 import { useLayoutEffect, useState } from "react";
+import Image from "next/image";
+
+import { endpoint } from "@/features/api";
 import { Seal } from "@/features/puzzle/select/Seal";
+import { Menubar } from "@/components/elements/Menubar";
+
+import { Puzzle } from "@/features/puzzle/types";
 import {
   GetAllPuzzleRequest,
   GetAllPuzzleResponse,
 } from "@/features/puzzle/types/get";
+
 import styles from "@/components/pages/PuzzleSelect/index.module.scss";
-import { Menubar } from "@/components/elements/Menubar";
 import BackgroundImage from "@/features/puzzle/select/images/puzzle-select-background.jpg";
-import Image from "next/image";
-import { Puzzle } from "@/features/puzzle/types";
 
 export const PuzzleSelect = () => {
   const [puzzles, setPuzzles] = useState<Puzzle[]>([]);
 
   useLayoutEffect(() => {
     const fetchPuzzles = async () => {
-      const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT;
-      if (baseUrl === undefined) {
-        alert("内部的なエラーが発生しました");
-        throw new Error('API endpoint is undefined. check ".env.local"');
-      }
       try {
         const req: GetAllPuzzleRequest = {};
-        // TODO:API完成時に書き換え
-        // const response = await fetch(`${baseUrl}/GetPuzzles`, {
-        //   method: "POST",
-        //   body: JSON.stringify(req),
-        // });
-        const response = await fetch(
-          "http://localhost:3000/api/puzzle/puzzles"
-        );
+        const response = await fetch(`${endpoint}/GetPuzzles`, {
+          method: "POST",
+          body: JSON.stringify(req),
+        });
+        // const response = await fetch(
+        //   "http://localhost:3000/api/puzzle/puzzles"
+        // );
         const json: GetAllPuzzleResponse = await response.json();
         if (json.response_status === "fail") {
           throw new Error(json.error);
